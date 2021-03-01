f0c447310d3bce23ff0b2e18248f6821049fd267
NathanMackenzie <weemackee@gmail.com>
2021-02-28 22:24:57 -0500

    working simulation

--- pisim.py
+++ pisim.py
@@ -2,17 +2,31 @@
 __version__ = '0.1.0'
 
 import math
+import random
 
 radius = 1
 
 origin_x = 0
 origin_y = 0
 
-x = .6
-y = .7
+in_count = 0
+out_count = 0
 
-dist = math.sqrt(x**2 + y**2)
 
-print(dist)
+
+for i in range(1000000):
+    x = (random.randint(0, 199)/100.0000)-1
+    y = (random.randint(0, 199)/100.0000)-1
+
+    dist = math.sqrt(x**2 + y**2)
+
+    if dist <= 1:
+        in_count += 1
+    else:
+        out_count += 1
+
+print("The estimated value of pi is...")
+
+print(4*(in_count/(out_count + in_count)))
 
 
