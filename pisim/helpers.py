3a51222ddf4fd19cdec84377204ed0860ee7fcfa
NathanMackenzie <weemackee@gmail.com>
2021-03-02 20:26:04 -0500

    changed function commenting to match PEP 257 -- Docstring Conventions

--- pisim/helpers.py
+++ pisim/helpers.py
@@ -1,28 +1,36 @@
 import math
 import random
 
-# get_random() provides random float between -1 and 1
-#
-# @return float between -1 and 1
+
 def get_random():
+    """
+    get_random provides random float between -1 and 1
+
+    :return: a random float between -1 and 1
+    """
     return (random.randrange(0, 200)/100.0000)-1
 
-# pythag(x, y) performs the pythagorean equation to calculate distance from 
-# origin on a cartesian coordinate plane
-#
-# @param x contains the x coordinate
-# @param y contains the y coordinate
-# @return distance of point from origin 
+
 def pythag(x, y):
+    """
+    pythag performs the pythagorean equation to calculate distance from the origin for a point on a 
+    2D cartesian coordinate plane
+
+    :param x: x coordinate of point
+    :param y: y coordinate of point
+    :return:  distance of point from the origin
+    """
     return math.sqrt(x**2 + y**2)
 
-# monte_carlo_sim(radius, instide, n) performs the monte carlo pi simulation equation
-#
-# @param radius contains the radius of the circle
-# @param inside is the number of points found to land inside the circle
-# @param n is the total number of simulation iterations
-# @return results containing the estimated calculation of pi
 def monte_carlo_sim(radius, inside, n):
+    """
+    monte_carol_sim calculates pi using the monte carlo pi simulation equation
+
+    :param radius: radius of circle
+    :param inside: number of point to land inside the circle
+    :param n: total number of points during simulation
+    :return: estimated value of pi
+    """
     area = 4 * radius # area of square surrounding circle
     result = area * (inside/n) 
     return result 
